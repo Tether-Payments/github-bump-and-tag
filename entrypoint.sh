8ab75259a1de80e8156f5a7b1ab5b93378271d15
Derek Sanford <derek@tetherpaymentsgroup.com>
2024-12-19 14:45:23 -0500

    bugfix(entrypoint): adding additional logging to find failure

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,16 +13,19 @@ git fetch --tags --recurse-submodules=no
 
 versionFmt="^v?[0-9]+\.[0-9]+\.[0-9]+$"
 version="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$versionFmt" | head -n 1)"
-
+echo "Found Version: ${version}"
 # Set default tag if none is found
 version="${version:="v0.0.0"}"
 setOutput "currentVersion" "$version"
 
 # Bump the patch version
 newVersion=$(echo "${version}" | awk -F. -v OFS=. '{$NF += 1 ; print}')
+echo "New Version: ${newVersion}"
 setOutput "newVersion" "$newVersion"
 
 # Set and push the new version tag
+echo "Tagging"
 git tag -f "$newVersion"
+echo "Pushing tag"
 git push -f origin "$newVersion"
 
