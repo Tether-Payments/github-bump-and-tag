14d136f70b3449f08081f28994ed49d17be1380e
Derek Sanford <derek@tetherpaymentsgroup.com>
2024-12-19 17:41:24 -0500

    chore(test): updating tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,10 +13,11 @@ echo "Fetching Tags"
 git fetch --tags --recurse-submodules=no
 
 # Find the current version
-echo "Finding current version t"
+echo "Finding current version"
 versionFmt="^v?[0-9]+\.[0-9]+\.[0-9]+$"
 echo "Running version finder"
 version="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$versionFmt" | head -n 1)"
+echo "Found it"
 echo "Found Version: ${version}"
 
 # Set default tag if none is found
