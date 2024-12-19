ed2112beae606736a8ccfa43a6741ada536a7497
Derek Sanford <derek@tetherpaymentsgroup.com>
2024-12-19 17:38:18 -0500

    chore(test): updating tests

--- entrypoint.sh
+++ entrypoint.sh
@@ -15,6 +15,7 @@ git fetch --tags --recurse-submodules=no
 # Find the current version
 echo "Finding current version"
 versionFmt="^v?[0-9]+\.[0-9]+\.[0-9]+$"
+echo "Running version finder"
 version="$(git for-each-ref --sort=-v:refname --format '%(refname:lstrip=2)' | grep -E "$versionFmt" | head -n 1)"
 echo "Found Version: ${version}"
 
